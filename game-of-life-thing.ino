13bb00c5c701ac4b20d6efd70789083b8c77f013
Steven Collins <StevenCollins@users.noreply.github.com>
2020-06-14 20:05:53 -0400

    Faster buffer update
    Data is grabbed in chunks,
    tweaked some math to use bitshifts,
    debug time displayed is now only calculation time, in microseconds,
    made some datatypes smaller.

--- game-of-life-thing.ino
+++ game-of-life-thing.ino
@@ -18,18 +18,20 @@ SSD1306Wire display(0x3c, SDA, SCL);
 
 // constants
 const bool DEBUG = true;
-const int X_RESOLUTION = 128; // actual screen x resolution
-const int Y_RESOLUTION = 64; // actual screen y resolution
-const unsigned int BUFFER_TYPE_SIZE = 32; // size of the type of the buffer
+const byte X_RESOLUTION = 128; // actual screen x resolution
+const byte Y_RESOLUTION = 64; // actual screen y resolution
+const byte BUFFER_TYPE_SIZE = 32; // size of the type of the buffer
+const byte BUFFER_TYPE_POWR = 5; // 2 ^ x = BUFFER_TYPE_SIZE
+const byte BUFFER_TYPE_MASK = 0b00011111; // BUFFER_TYPE_MASK >> BUFFER_TYPE_BITS == 0
 unsigned long BUFFER[2][X_RESOLUTION/BUFFER_TYPE_SIZE][Y_RESOLUTION]; // two buffers, each with enough bits for each pixel. for mental reasons, x is the direction with the datatype bits.
 // almost constants
-int CELL_SIZE = 1; // desired size of cell in pixels
-int WIDTH = X_RESOLUTION / CELL_SIZE; // apparent screen x resolution
-int HEIGHT = Y_RESOLUTION / CELL_SIZE; // apparent screen x resolution
-int TARGET_FRAMETIME = 16; // delay if a frame takes less time than this
+byte CELL_SIZE = 1; // desired size of cell in pixels
+byte WIDTH = X_RESOLUTION / CELL_SIZE; // apparent screen x resolution
+byte HEIGHT = Y_RESOLUTION / CELL_SIZE; // apparent screen x resolution
+byte TARGET_FRAMETIME = 16000; // delay if a frame takes less time than this
 
 // global variables
-int frontBuffer = 0;
+byte frontBuffer = 0;
 
 void setup() {
   // setup serial, if debugging
@@ -73,68 +75,86 @@ void setup() {
 
 void loop() {
   // important things first
-  int backBuffer = frontBuffer == 1 ? 0 : 1;
-  unsigned long frameStartTime = millis();
+  unsigned long frameStartTime = micros();
+  byte backBuffer = frontBuffer == 1 ? 0 : 1;
 
   // handle requests
   server.handleClient();
   MDNS.update();
 
   // update the back buffer
+  unsigned long bufferStartTime = micros();
   for (int y = 0; y < HEIGHT; y++) {
     int yminusone = y == 0 ? HEIGHT - 1 : y - 1;
     int yplusone = y == HEIGHT - 1 ? 0 : y + 1;
     for (int x = 0; x < WIDTH; x++) {
       int liveNeighbours = 0;
       
-      // get neighbours above and below
-      int xactual = x / BUFFER_TYPE_SIZE;
-      int xoffset = x % BUFFER_TYPE_SIZE;
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xactual][yminusone], xoffset);
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xactual][yplusone], xoffset);
-
-      // get leftside neighbours
+      int xactual = x >> BUFFER_TYPE_POWR;
+      int xoffset = x & BUFFER_TYPE_MASK;
       int xminusone = x == 0 ? WIDTH - 1 : x - 1;
-      int xminusoneactual = xminusone / BUFFER_TYPE_SIZE;
-      int xminusoneoffset = xminusone % BUFFER_TYPE_SIZE;
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][yminusone], xminusoneoffset);
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][y], xminusoneoffset);
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][yplusone], xminusoneoffset);
-
-      // get rightside neighbours
+      int xminusoneactual = xminusone >> BUFFER_TYPE_POWR;
+      int xminusoneoffset = xminusone & BUFFER_TYPE_MASK;
       int xplusone = x == WIDTH - 1 ? 0 : x + 1;
-      int xplusoneactual = xplusone / BUFFER_TYPE_SIZE;
-      int xplusoneoffset = xplusone % BUFFER_TYPE_SIZE;
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][yminusone], xplusoneoffset);
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][y], xplusoneoffset);
-      liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][yplusone], xplusoneoffset);
+      int xplusoneactual = xplusone >> BUFFER_TYPE_POWR;
+      int xplusoneoffset = xplusone & BUFFER_TYPE_MASK;
 
-      int thisCell = bitRead(BUFFER[frontBuffer][xactual][y], xoffset);
+      // get bytes from the buffer - elses below fallback if the byte doesn't contain the correct data
+      unsigned long topRow = BUFFER[frontBuffer][xactual][yminusone];
+      unsigned long midRow = BUFFER[frontBuffer][xactual][y];
+      unsigned long botRow = BUFFER[frontBuffer][xactual][yplusone];
+
+      // get neighbours above and below
+      liveNeighbours += bitRead(topRow, xoffset); // neighbour above
+      liveNeighbours += bitRead(botRow, xoffset); // neighbour below
+      // left side neighbours
+      if (xminusoneactual == xactual) {
+        liveNeighbours += bitRead(topRow, xminusoneoffset);
+        liveNeighbours += bitRead(midRow, xminusoneoffset);
+        liveNeighbours += bitRead(botRow, xminusoneoffset);
+      } else {
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][yminusone], xminusoneoffset);
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][y], xminusoneoffset);
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xminusoneactual][yplusone], xminusoneoffset);
+      }
+      // right side neighbours
+      if (xplusoneactual == xactual) {
+        liveNeighbours += bitRead(topRow, xplusoneoffset);
+        liveNeighbours += bitRead(midRow, xplusoneoffset);
+        liveNeighbours += bitRead(botRow, xplusoneoffset);
+      } else {
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][yminusone], xplusoneoffset);
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][y], xplusoneoffset);
+        liveNeighbours += bitRead(BUFFER[frontBuffer][xplusoneactual][yplusone], xplusoneoffset);
+      }
+
+      bool thisCell = bitRead(midRow, xoffset); // the current cell
       bitWrite(BUFFER[backBuffer][xactual][y], xoffset, getNewState(thisCell, liveNeighbours));
     }
   }
+  unsigned long bufferEndTime = micros();
 
   // send the front buffer to the display
   display.clear();
   display.setColor(WHITE);
   for (int y = 0; y < HEIGHT; y++) {
-    for (int x = 0; x < WIDTH / BUFFER_TYPE_SIZE; x++) {
+    for (int x = 0; x < (WIDTH >> BUFFER_TYPE_POWR); x++) {
       unsigned long cellGroup = BUFFER[frontBuffer][x][y];
       for (int i = 0; i < BUFFER_TYPE_SIZE; i++) {
         if (bitRead(cellGroup, i) == 1) {
-          display.fillRect((x * BUFFER_TYPE_SIZE + i) * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+          display.fillRect(((x << BUFFER_TYPE_POWR) + i) * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
       }
     }
   }
-  if (DEBUG) display.drawString(0, 0, String(millis() - frameStartTime)); // draw frametime
+  if (DEBUG) display.drawString(0, 0, String(bufferEndTime - bufferStartTime)); // draw buffer calculation time
   display.display();
 
   // swap the buffers
   frontBuffer = backBuffer;
 
   // delay to make the FPS consistent
-  while ((millis() - frameStartTime) < TARGET_FRAMETIME);
+  while ((micros() - frameStartTime) < TARGET_FRAMETIME);
 }
 
 // return the new state for a cell, based on Conway's Game of Life rules
